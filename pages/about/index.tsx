ac2041aa8e32b840124f9e06419e0e06f9c73525
Valentina Panic <valentina.panic87@gmail.com>
2023-01-08 15:36:42 -0700

    resume page

---
+++ pages/about/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import React from "react";
+
+ const About: NextPage = () => {
+  return (
+    <>
+      <div>This Page is About me</div>
+    </>
+  );
+};
+export default About;
