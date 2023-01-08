5b36f49f728d332a1b5e1273875431420b488933
Valentina Panic <valentina.panic87@gmail.com>
2023-01-08 15:37:14 -0700

    resume page

---
+++ pages/resume/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import React from "react";
+
+ const Resume: NextPage = () => {
+  return (
+    <>
+      <div>This Page is Resume me</div>
+    </>
+  );
+};
+export default Resume;
