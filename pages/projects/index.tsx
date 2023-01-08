dc78726599f97e38d5814fe56542cda7bee7425a
Valentina Panic <valentina.panic87@gmail.com>
2023-01-08 15:36:51 -0700

    projects

---
+++ pages/projects/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import React from "react";
+
+ const Projects: NextPage = () => {
+  return (
+    <>
+      <div>This Page is Projects me</div>
+    </>
+  );
+};
+export default Projects;
